d7724c1ecf0b37be99e79ccb23f5bde16161b502
Chawengwit <chawengwit@taximail.com>
2026-01-12 15:19:16 +0700

    Sets up basic dashboard layout
    Initializes the dashboard layout with a sidebar and content area, providing a basic structure for future components.

---
+++ app/dashboard/@sidebar/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div className="overflow-auto max-h-screen p-2 flex-none w-1/3">
+            <div>Signed in as: email</div>
+        </div>
+    );
+
+    
+}
