a24aca4ce1d8914d9f56e78fa48556d63ad22168
Chawengwit <chawengwit@taximail.com>
2026-01-19 17:33:30 +0700

    Transforms note dates using Luxon
    Uses Luxon library to handle and transform date and time
    information of notes.
    
    This provides a more robust and flexible way to work with dates
    compared to the native JavaScript Date object.

---
+++ app/lip/client/api.ts
@@ -0,0 +1,32 @@
+import { DateTime } from "luxon";
+import { NoteData } from "./types";
+
+export async function fetchNotes(parent_note_id?: string) {
+    let queryString = "";
+    if(parent_note_id) {
+        queryString = `?parent_note_id=${parent_note_id}`;
+    }
+
+    const notesRes = await fetch("api/notes" + queryString, {
+        cache: "no-store",
+    });
+
+    const json = await notesRes.json();
+    console.log("json >> ", json);
+
+    const transformedJson = json.map((note: any) => {
+        return transformJsonToNote(note);
+    });
+
+    return transformedJson;
+}
+
+function transformJsonToNote(note: any): NoteData {
+    return {
+        ...note,
+        created_at: DateTime.fromISO(note.created_at),
+        updated_at: DateTime.fromISO(note.updated_at),
+        child_notes: [],
+        child_count: 0      
+    };
+}
